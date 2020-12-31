bfc67313a49cb258de4ae4207b0bdffbbcbf1484
Hondo <hi@hondo.xyz>
2020-12-30 19:56:45 -0500

    Adds card component example

---
+++ card-component/card.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+	title: "Cards",
+    status: "ready",
+    preview: "@centered_grey",
+    "variants": [
+		{
+            "name": "default",
+            "label": "Link",
+            "context": {
+                "title": "This is the title",
+                "url": "https://new.mta.info",
+                "aria_label": "Click to open Brooklyn Bus Network Redesign Poster (Multiple Languages) link"
+        	}
+		},
+		{
+			"name": "Image",
+            "status": "ready",
+            "context": {
+        	    "title": "This is the title",
+                "url": "https://new.mta.info",
+                "aria_label": "Click to open Brooklyn Bus Network Redesign Poster (Multiple Languages) link",
+                "image": "http://local.metrota2mr.com/sites/default/files/styles/landing_page_thumbnail/public/2019-10/English-Spanish%20Crop.PNG?itok=EBrTvZPH"
+        	}
+        },
+        {
+			"name": "Subtitle",
+            "status": "ready",
+            "context": {
+        	    "title": "This is the title",
+                "url": "https://new.mta.info",
+                "subtitle": "Subtitle",
+                "description": "Click to open Brooklyn Bus Network Redesign Poster (Multiple Languages) link",
+                "aria_label": "Click to open Brooklyn Bus Network Redesign Poster (Multiple Languages) link",
+                "image": "http://local.metrota2mr.com/sites/default/files/styles/landing_page_thumbnail/public/2019-10/English-Spanish%20Crop.PNG?itok=EBrTvZPH"
+        	}
+		},
+        {
+			"name": "Description",
+            "status": "ready",
+            "context": {
+        	    "title": "This is the title",
+                "url": "https://new.mta.info",
+                "subtitle": "Subtitle",
+                "description": "Click to open Brooklyn Bus Network Redesign Poster (Multiple Languages) link",
+                "aria_label": "Click to open Brooklyn Bus Network Redesign Poster (Multiple Languages) link",
+                "image": "http://local.metrota2mr.com/sites/default/files/styles/landing_page_thumbnail/public/2019-10/English-Spanish%20Crop.PNG?itok=EBrTvZPH"
+        	}
+		}
+	]
+};
